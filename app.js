83682affb3073618f7fae0dc975265aeeb9ba01e
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-10-18 11:56:08 +0300

    Connect database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
@@ -8,8 +10,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "test" });
-});
+async function start() {
+  try {
+    await mongoose.connect(process.env.mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log("We are live on " + PORT);
+    });
+  } catch (e) {
+    console.log("Server error:", e.message);
+  }
+}
 
-app.listen(PORT, () => console.log("Server is running on port " + PORT));
+start();
