d36cf178227ede5bfd72a589c52d9db104a1a214
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-11-27 16:00:51 +0300

    Add logs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,12 +7,15 @@ module.exports = async function (req, res, next) {
 
     const token =
       req.cookies["access token"] || req.headers.authorization?.split(" ")[1];
+    console.log("token: ", token);
     if (!token) throw new Error("No token provided");
     const data = jwt.verify(token, process.env.SECRET);
+    console.log("data: ", data);
     req.decoded = data;
 
     return next();
   } catch (error) {
+    console.log('Not auth');
     console.log(error.message);
     return res.status(401).send({
       error: "Not Authenticated",
