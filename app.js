cfb2bc96a67cfff86b721f98d9b828e4146503d1
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-05 17:57:02 +0300

    Update transporter config

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ const transporter = nodemailer.createTransport({
   service: process.env.SMTP_SERVICE,
   host: "smtp.gmail.com",
   port: 465,
-  secure: true,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASSWORD,
